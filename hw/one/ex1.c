dfcb6f80e4fdd1f87c43cd4dce6f20d800fde8f9
connor wool <benjamin.wool@wsu.edu>
2017-01-20 10:43:52 -0800

    almost done with stack frame coding

---
+++ hw/one/ex1.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+int *FP;
+
+int main (int argc, char *argv[], char *env[])
+{
+	int a,b,c;
+	printf("enter main\n");
+	a=1; b=2; c=3;
+	A(a,b);
+	printf("exit main\n");
+}
+
+int A(int x, int y)
+{
+	int d,e,f;
+	printf("enter A\n");
+	d=4; e=5; f=6;
+	B(d,e);
+	printf("exit A\n");
+}
+
+int B(int x, int y)
+{
+	int u,v,w;
+	printf("enter B\n");
+	u=7; v=8; w=9;
+	
+	//this asm call loads stack pointer into global "FP"
+	asm("movl %ebp, FP");
+
+	/*****
+	"By convention, the CPU's FP = 0 when crt0.o is entered
+		from the OS kernel. So the stack frame link
+		list ends with a 0. When a function returns,
+		its stack frame is deallocated and the stack
+		shrinks back" - MTX Design, pg. 27
+	*****/	
+	
+	//write code to print the stack frame linked list here:
+	printf("\nprint the stack frame linked list:\n");
+	while(FP != 0)
+	{
+		printf("FP = %d\t*FP = %d\n",FP,*FP);
+		FP = *FP;
+	}	
+
+	//print in HEX the address and contents of the stack from
+	//FP to the stack frame of main()
+	
+	//reset the value of stack frame pointer to FP	
+	asm("movl %ebp, FP");
+
+	printf("\nprint stack address and contents:\n");
+	while(*FP != 0)
+	{
+		printf("addr: %x\t\tcontents: %d\n",FP,*FP);
+		FP -= 1;
+	}
+
+	printf("exit B\n");
+}
